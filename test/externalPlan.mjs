e7ec167eb2e3f0d5e3b7dac4ea5c113ae79782c9
Colton Loewen <loewencjay@gmail.com>
2018-10-23 20:11:05 -0600

    Finished resolve

---
+++ test/externalPlan.mjs
@@ -0,0 +1,4 @@
+export default {
+	external: true,
+	execute: () => {}
+};
--- test/resolveSpec.mjs
+++ test/resolveSpec.mjs
@@ -1,19 +1,26 @@
 import jasmine from "./jasmine";
 import { resolve } from "../lib/index.mjs";
 
-const { describe, it, pending } = jasmine.env;
+const { describe, it, expect } = jasmine.env;
 
 describe("resolve", () => {
-	it("does not mutate valid plans", () => {
-		pending("unfinished test");
+	it("does not mutate valid plans", async () => {
+		let plan = { execute: () => {}, plans: [] };
+		expect(await resolve(plan)).toBe(plan);
 	});
-	it("imports external plans", () => {
-		pending("unfinished test");
+	it("does not resolve builtin modules", async (done) => {
+		resolve("path").then(() => done.fail(), () => done());
 	});
-	it("throws for invalid plans", () => {
-		pending("unfinished test");
+	it("imports external plans", async () => {
+		expect((await resolve("./externalPlan.mjs")).external).toBe(true);
 	});
-	it("throws for invalid actions", () => {
-		pending("unfinished test");
+	it("must add the mjs extension if an extension is not present", async () => {
+		expect((await resolve("./externalPlan")).external).toBe(true);
+	});
+	it("throws for invalid plans", (done) => {
+		resolve({ plans: Symbol() }).then(() => done.fail(), () => done());
+	});
+	it("throws for invalid actions", (done) => {
+		resolve({ execute: Symbol() }).then(() => done.fail(), () => done());
 	});
 });

7844b08f3a307725fadaada05ca76cded09e0ff0
Colton Loewen <loewencjay@gmail.com>
2018-10-24 22:55:48 -0600

    Finished static resources

--- test/staticResourcesSpec.mjs
+++ test/staticResourcesSpec.mjs
@@ -1,24 +1,58 @@
 import jasmine from "./jasmine";
 import { setResource, execute } from "../lib/index.mjs";
 
-const { describe, it, pending } = jasmine.env;
+const { describe, it, expect } = jasmine.env;
 
 describe("static resources", () => {
 	describe("setResource", () => {
 		it("sets a resource", () => {
-			pending("unfinished test");
+			setResource("setTest", true);
 		});
 	});
-	it("does not include inexistant resources", () => {
-		pending("unfinished test");
+	it("does not include inexistant resources", async (done) => {
+		await execute({
+			include: ["inexistant"],
+			execute: (inexistant) => {
+				expect(inexistant).toBe(undefined);
+			}
+		});
+		done();
 	});
-	it("includes a single resource", () => {
-		pending("unfinished test");
+	it("includes a single resource", async (done) => {
+		setResource("one", 1);
+		setResource("two", 2);
+		await execute({
+			include: ["one"],
+			execute: (one, two) => {
+				expect(one).toBe(1);
+				expect(two).toBe(undefined);
+			}
+		});
+		done();
 	});
-	it("includes multiple resources", () => {
-		pending("unfinished test");
+	it("includes multiple resources", async (done) => {
+		setResource("one", 1);
+		setResource("two", 2);
+		await execute({
+			include: ["one", "two"],
+			execute: (one, two) => {
+				expect(one).toBe(1);
+				expect(two).toBe(2);
+			}
+		});
+		done();
 	});
-	it("includes changed resources", () => {
-		pending("unfinished test");
-	})
+	it("includes changed resources", async (done) => {
+		setResource("one", 1);
+		setResource("two", 2);
+		setResource("two", 3);
+		await execute({
+			include: ["one", "two"],
+			execute: (one, two) => {
+				expect(one).toBe(1);
+				expect(two).toBe(3);
+			}
+		});
+		done();
+	});
 });
